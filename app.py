717099a90dbced8b09dcc36808f0117c2e6ca06f
harika-maha <81984852+harika-maha@users.noreply.github.com>
2021-04-11 21:34:05 +0530

    Updated app.py
    Established MongoDB connection and retrieved data from the form

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, url_for
 from flask_pymongo import PyMongo
+import pymongo
 
+mongo_uri = "mongodb://localhost:27017/"
+client = pymongo.MongoClient(mongo_uri)
+mydb = client.formDB
+mycoll = mydb.people
 app = Flask(__name__)
-app.config['MONGO_URI']= 'mongodb://localhost:27017/'
-mongo=PyMongo(app)
 @app.route('/')
 
 def form():
@@ -11,7 +14,9 @@ def form():
 
 @app.route('/form', methods=["POST"])
 def redirect():
-	if 'Name' in request.files:
-		name = request.files['Name']
-		mongo.db.users.insert({'Name':request.form.get('Name'),'Gender':request.form.get("Gender"),'email':request.form.get('email'),'pno':request.form.get('pno')})
+	name = request.form.get("Name")
+	gender = request.form.get("Gender")
+	email = request.form.get("email")
+	ph_n = request.form.get("pno")
+	mongo.db.users.insert({'Name':request.form.get('Name'),'Gender':request.form.get("Gender"),'email':request.form.get('email'),'pno':request.form.get('pno')})
 	return render_template("redirect.html")
